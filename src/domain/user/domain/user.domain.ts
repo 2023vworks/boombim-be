3f8a3b62692c9b9613a163460c8b9351d7c6d622
jae hyun <1994dbwogus@gmail.com>
2023-10-04 23:46:23 +0900

    feat: 도메인 객체 User 수정
    제거 필드
    - lastFeedWrittenAt
    
    추가 필드
    - isRechargeStart
    - feedWritingCountRechargeStartAt
    - isMaxFeedWritingCount
    
    수정
    - nickname
    - createNickname => generateNickname
    - renewFeedWritingCount: 피드 작성 횟수 갱신 알고리즘 변경

--- src/domain/user/domain/user.domain.ts
+++ src/domain/user/domain/user.domain.ts
@@ -6,54 +6,127 @@ import { BaseDomain } from 'src/domain/base.domain';
 export interface UserProps extends UserEntity {}
 
 export class User extends BaseDomain<UserProps> {
+  private MAX_FEED_WRITING_COUNT = 5;
+
   constructor(readonly props: UserProps) {
     super(props);
   }
 
+  /**
+   * MBTI 유형
+   */
   get mbtiType(): string {
     return this.props.mbtiType;
   }
 
+  /**
+   * 닉네임
+   */
   get nickname(): string {
-    return this.id ? this.createNickname() : '0000';
+    return this.id ? this.props.nickname : '0000';
   }
 
+  /**
+   * JWT 토큰
+   */
   get token(): string {
     return this.props.token;
   }
 
+  /**
+   * 마지막 접속일
+   * - mvp에서는 생성시 추가
+   */
   get accessedAt(): Date {
     return this.props.accessedAt;
   }
 
   /**
-   * 피드 작성 횟수
-   * - 최대가 5개 이며, 소모시 시간당 1개가 충전된다.
+   * 피드 작성 가능 횟수
+   * - min 0
+   * - max 5
    */
   get feedWritingCount(): number {
-    const { feedWritingCount, lastFeedWrittenAt } = this.props;
-    if (!lastFeedWrittenAt || feedWritingCount === 5) return 5;
-
-    const differenceHours = DateUtil.differenceInHours(lastFeedWrittenAt);
-    return differenceHours > 5 ? 5 : differenceHours;
+    return this.props.feedWritingCount;
   }
 
-  get lastFeedWrittenAt(): Date | null {
-    return this.props.lastFeedWrittenAt;
+  /**
+   * 충전 시작 여부
+   */
+  get isRechargeStart(): boolean {
+    return this.props.isRechargeStart;
   }
 
-  renewFeedWritingCount() {
-    return this.feedWritingCount;
+  /**
+   * feedWritingCount(피드 작성 가능 횟수) 충전 시작 시간
+   */
+  get feedWritingCountRechargeStartAt(): Date {
+    return this.props.feedWritingCountRechargeStartAt;
   }
 
-  private createNickname(): string {
+  /* ============ custom ============ */
+  /**
+   * 피드 최대 작성 횟수인지 확인합니다.
+   * - 최대 5회까지 작성 가능합니다.
+   */
+  get isMaxFeedWritingCount(): boolean {
+    return this.props.feedWritingCount === this.MAX_FEED_WRITING_COUNT;
+  }
+
+  /**
+   * 피드 작성 횟수를 갱신합니다.
+   * - 작성횟수는 feedWritingCountRechargeStartAt 부터 dateLeft 시간까지 1시간에 1씩 충전됩니다.
+   * - 최대 5회까지 작성 가능합니다.
+   * - dateLeft는 기본적으로 현재시간 기준으로 합니다.
+   * @param dateLeft
+   * @returns
+   * @error {Error} 충전횟수가 0보다 작을 경우 에러를 발생시킵니다.
+   */
+  renewFeedWritingCount(dateLeft: Date = new Date()) {
+    if (this.isMaxFeedWritingCount) return this;
+
+    const differenceHours = DateUtil.differenceInHours(
+      dateLeft,
+      this.props.feedWritingCountRechargeStartAt,
+    );
+    // 음수면 에러
+    if (differenceHours < 0)
+      throw new Error('[UserDomain] differenceHours is less than 0');
+    // 갱신할 필요 없으면 그냥 리턴
+    if (differenceHours === 0) return this;
+
+    // 피드 작성 횟수 갱신
+    const newFeedWritingCount = this.props.feedWritingCount + differenceHours;
+    this.props.feedWritingCount =
+      newFeedWritingCount > this.MAX_FEED_WRITING_COUNT
+        ? this.MAX_FEED_WRITING_COUNT
+        : newFeedWritingCount;
+    // 피드 작성 횟수 충전 시작 시간 갱신
+    this.props.feedWritingCountRechargeStartAt = this.isMaxFeedWritingCount
+      ? this.props.feedWritingCountRechargeStartAt
+      : new Date();
+    // 충전 시작 여부 갱신
+    this.props.isRechargeStart = this.isMaxFeedWritingCount ? false : true;
+
+    return this;
+  }
+
+  /**
+   * 닉네임
+   * - 000 + id,
+   * - default 0000
+   */
+  generateNickname(): this {
     switch (this.id.toString().length) {
       case 1:
-        return `00${this.id}`;
+        this.props.nickname = `00${this.id}`;
+        return this;
       case 2:
-        return `0${this.id}`;
+        this.props.nickname = `0${this.id}`;
+        return this;
       default:
-        return `${this.id}`;
+        this.props.nickname = `${this.id}`;
+        return this;
     }
   }
 }
