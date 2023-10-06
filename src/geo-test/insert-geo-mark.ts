cf98d7ccb6052bf0d9e9e75aa864331584973228
jae hyun <1994dbwogus@gmail.com>
2023-10-07 01:18:49 +0900

    chore: 테스트 데이터 생성 스크립트 수정

--- src/geo-test/insert-geo-mark.ts
+++ src/geo-test/insert-geo-mark.ts
@@ -47,6 +47,10 @@ const address: KakaoGeoAddresses[] = JSON.parse(
 AppDataSource.initialize()
   .then(async () => {
     console.log('Data Source has been initialized!');
+
+    // const result = createGeoMark(regionInfo[0], address[0]);
+    // console.log(result);
+
     const feedRepo = AppDataSource.getRepository(FeedEntity);
     const useRepo = AppDataSource.getRepository(UserEntity);
     const user = useRepo.create({
@@ -91,45 +95,56 @@ function createGeoMark(
   addresses: KakaoGeoAddresses,
 ): GeoMarkEntity {
   const { address, road_address } = addresses;
-  const result: GeoMarkEntity = Util.toInstance(GeoMarkEntity, {
-    x: +regionInfo.x,
-    y: +regionInfo.y,
-    type: CoordType.WGS84,
-    srid: 4326,
-    regionInfo: {
-      regionType: regionInfo.region_type,
-      code: regionInfo.code,
-      addressName: regionInfo.address_name,
-      region1depthName: regionInfo.region_1depth_name,
-      region2depthName: regionInfo.region_2depth_name,
-      region3depthName: regionInfo.region_3depth_name,
-      region4depthName: regionInfo.region_4depth_name,
+  const repo = AppDataSource.getRepository(GeoMarkEntity);
+
+  const result: GeoMarkEntity = Util.toInstance(
+    GeoMarkEntity,
+    {
       x: +regionInfo.x,
       y: +regionInfo.y,
+      type: CoordType.WGS84,
+      point: {
+        type: 'Point',
+        coordinates: [+regionInfo.x, +regionInfo.y],
+      },
+      srid: 4326,
+      regionInfo: {
+        regionType: regionInfo.region_type as any,
+        code: regionInfo.code,
+        addressName: regionInfo.address_name,
+        region1DepthName: regionInfo.region_1depth_name,
+        region2DepthName: regionInfo.region_2depth_name,
+        region3DepthName: regionInfo.region_3depth_name,
+        region4DepthName: regionInfo.region_4depth_name,
+        x: +regionInfo.x,
+        y: +regionInfo.y,
+      },
+      address: {
+        addressName: address.address_name,
+        region1DepthName: address.region_1depth_name,
+        region2DepthName: address.region_2depth_name,
+        region3DepthName: address.region_3depth_name,
+        mountainYn: address.mountain_yn as any,
+        mainAddressNo: address.main_address_no,
+        subAddressNo: address.sub_address_no,
+      },
+      roadAddress: road_address && {
+        addressName: road_address.address_name,
+        region1DepthName: road_address.region_1depth_name,
+        region2DepthName: road_address.region_2depth_name,
+        region3DepthName: road_address.region_3depth_name,
+        roadName: road_address.road_name,
+        undergroundYn: road_address.underground_yn,
+        mainBuildingNo: road_address.main_building_no,
+        subBuildingNo: road_address.sub_building_no,
+        buildingName: road_address.building_name,
+        zoneNo: road_address.zone_no,
+      },
     },
-    address: {
-      addressName: address.address_name,
-      region1depthName: address.region_1depth_name,
-      region2depthName: address.region_2depth_name,
-      region3depthName: address.region_3depth_name,
-      mountainYn: address.mountain_yn,
-      mainAddressNo: address.main_address_no,
-      subAddressNo: address.sub_address_no,
-      zipCode: address.zip_code,
+    {
+      ignoreDecorators: true,
     },
-    roadAddress: road_address && {
-      addressName: road_address.address_name,
-      region1depthName: road_address.region_1depth_name,
-      region2depthName: road_address.region_2depth_name,
-      region3depthName: road_address.region_3depth_name,
-      roadName: road_address.road_name,
-      undergroundYn: road_address.underground_yn,
-      mainBuildingNo: road_address.main_building_no,
-      subBuildingNo: road_address.sub_building_no,
-      buildingName: road_address.building_name,
-      zoneNo: road_address.zone_no,
-    },
-  });
+  );
 
   return {
     ...result,
