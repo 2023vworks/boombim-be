57155649df8fa3f91657ef087e075fceb37e6975
jae hyun <1994dbwogus@gmail.com>
2023-10-07 08:22:09 +0900

    feat: User 도메인 객체 메서드 추가 및 개선

--- src/domain/user/domain/user.domain.ts
+++ src/domain/user/domain/user.domain.ts
@@ -1,5 +1,6 @@
-import { DateUtil } from '@app/common';
+import { DateUtil, errorMessage } from '@app/common';
 import { UserEntity } from '@app/entity';
+import { ConflictException } from '@nestjs/common';
 
 import { BaseDomain } from 'src/domain/base.domain';
 
@@ -7,52 +8,33 @@ export interface UserProps extends UserEntity {}
 
 export class User extends BaseDomain<UserProps> {
   private MAX_FEED_WRITING_COUNT = 5;
-  private oldFeedWritingCount: number;
+  private lastRechargeStartAt: Date;
 
   constructor(readonly props: UserProps) {
     super(props);
+    this.lastRechargeStartAt = props.feedWritingCountRechargeStartAt;
   }
 
-  /**
-   * MBTI 유형
-   */
   get mbtiType(): string {
     return this.props.mbtiType;
   }
 
-  /**
-   * 닉네임
-   */
   get nickname(): string {
     return this.id ? this.props.nickname : '0000';
   }
 
-  /**
-   * JWT 토큰
-   */
   get token(): string {
     return this.props.token;
   }
 
-  /**
-   * 마지막 접속일
-   * - mvp에서는 생성시 추가
-   */
   get accessedAt(): Date {
     return this.props.accessedAt;
   }
 
-  /**
-   * 피드 작성 가능 횟수
-   * - min 0, max 5
-   */
   get feedWritingCount(): number {
     return this.props.feedWritingCount;
   }
 
-  /**
-   * feedWritingCount(피드 작성 가능 횟수) 충전 시작 시간
-   */
   get feedWritingCountRechargeStartAt(): Date {
     return this.props.feedWritingCountRechargeStartAt;
   }
@@ -60,15 +42,14 @@ export class User extends BaseDomain<UserProps> {
   /* ============ custom ============ */
 
   /**
-   * 피드 최대 작성 횟수인지 확인합니다.
-   * - 최대 5회까지 작성 가능합니다.
+   * 피드 작성 횟수가 최대치인지 확인합니다.
    */
   get isMaxFeedWritingCount(): boolean {
     return this.props.feedWritingCount === this.MAX_FEED_WRITING_COUNT;
   }
 
   /**
-   * 충전 시작 여부
+   * 피드 작성 횟수가 5회 이하인지 확인합니다.
    */
   get isRechargeStart(): boolean {
     return this.props.feedWritingCount <= this.MAX_FEED_WRITING_COUNT;
@@ -78,44 +59,16 @@ export class User extends BaseDomain<UserProps> {
    * 피드 작성 횟수가 갱신되었는지 확인합니다.
    */
   get isRenewedFeedWritingCount(): boolean {
-    return this.oldFeedWritingCount !== this.props.feedWritingCount;
-  }
-
-  /**
-   * 피드 작성 횟수를 갱신합니다.
-   * - 작성횟수는 feedWritingCountRechargeStartAt 부터 dateRight 시간까지 1시간에 1씩 충전됩니다.
-   * - 최대 5회까지 작성 가능합니다.
-   * - dateRight 기본적으로 현재시간 기준으로 합니다.
-   * @param dateRight
-   * @returns
-   * @error {Error} 충전횟수가 0보다 작을 경우 에러를 발생시킵니다.
-   */
-  renewFeedWritingCount(dateRight: Date = new Date()) {
-    const differenceHours = DateUtil.differenceInHours(
-      this.props.feedWritingCountRechargeStartAt,
-      dateRight,
+    return (
+      this.lastRechargeStartAt !== this.props.feedWritingCountRechargeStartAt
     );
-    if (differenceHours < 0)
-      throw new Error('[UserDomain] differenceHours is less than 0');
-    // 갱신할 필요 없으면 그냥 리턴
-    if (differenceHours === 0) return this;
-
-    this.oldFeedWritingCount = this.props.feedWritingCount;
-
-    // 피드 작성 횟수 갱신
-    const newFeedWritingCount = this.props.feedWritingCount + differenceHours;
-    this.props.feedWritingCount =
-      newFeedWritingCount > this.MAX_FEED_WRITING_COUNT
-        ? this.MAX_FEED_WRITING_COUNT
-        : newFeedWritingCount;
-
-    // 피드 작성 횟수 충전 시작 시간 갱신
-    this.props.feedWritingCountRechargeStartAt = this.isRenewedFeedWritingCount
-      ? new Date()
-      : this.props.feedWritingCountRechargeStartAt;
-
-    return this;
   }
+
+  get canWriteFeed(): boolean {
+    return this.props.feedWritingCount > 0;
+  }
+
+  /* ================== method ================== */
 
   /**
    * 닉네임
@@ -134,5 +87,64 @@ export class User extends BaseDomain<UserProps> {
         this.props.nickname = `${this.id}`;
         return this;
     }
+  }
+
+  /**
+   * 피드 작성 횟수 소모
+   * 1. 피드 작성 횟수 갱신
+   * 2. 가능 여부 확인하여 불가능하면 에러 발생
+   * 3. 횟수를 차감
+   * @returns
+   * @Error {ConflictException} 피드 작성 횟수가 0일 경우 에러를 발생시킵니다.
+   */
+  useWritingCount() {
+    this.renewFeedWritingCount();
+    if (!this.canWriteFeed)
+      throw new ConflictException(errorMessage.E409_FEED_001);
+
+    this.props.feedWritingCount--;
+    return this;
+  }
+
+  /**
+   * 피드 작성 횟수를 갱신합니다.
+   * - 작성횟수는 feedWritingCountRechargeStartAt 부터 dateRight 시간까지 1시간에 1씩 충전됩니다.
+   * - 최대 5회까지 작성 가능합니다.
+   * - dateRight 기본적으로 현재시간 기준으로 합니다.
+   * @param dateRight
+   * @returns
+   * @error {Error} 충전횟수가 0보다 작을 경우 에러를 발생시킵니다.
+   */
+  renewFeedWritingCount(dateRight: Date = new Date()) {
+    const hoursPassed = DateUtil.differenceInHours(
+      this.props.feedWritingCountRechargeStartAt,
+      dateRight,
+    );
+    if (hoursPassed < 0)
+      throw new Error('[UserDomain] differenceHours is less than 0');
+    // 갱신할 필요 없으면 그냥 리턴
+    if (hoursPassed === 0) return this;
+
+    const lastFeedWritingCount = this.props.feedWritingCount;
+    this.props.feedWritingCount = Math.min(
+      this.MAX_FEED_WRITING_COUNT,
+      lastFeedWritingCount + hoursPassed,
+    );
+
+    const isRenewed = lastFeedWritingCount !== this.props.feedWritingCount;
+    if (isRenewed) {
+      // 피드 작성 횟수 충전 시작 시간 갱신
+      this.setLastRechargeStartAt(this.props.feedWritingCountRechargeStartAt);
+      this.props.feedWritingCountRechargeStartAt = DateUtil.addHours(
+        this.props.feedWritingCountRechargeStartAt,
+        hoursPassed,
+      );
+    }
+
+    return this;
+  }
+
+  private setLastRechargeStartAt(date: Date) {
+    this.lastRechargeStartAt = date;
   }
 }
