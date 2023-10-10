96b5018c1a4d132137368da522e99b702c7941b8
jae hyun <1994dbwogus@gmail.com>
2023-10-10 21:51:55 +0900

    refactor: winston.config 운영 환경 로그 변경
    - 클라우드 환경에서 log를 날짜별 파일로 관리할 필요가 없다고 판단되어 개발 운영 동일하게 로그를 콘솔로 출력하도록 변경
    - 로그 관리 코드에서가 아닌 인프라적으로 관리할 예정이다.
    ( Ex. 표준 입출력 파일로 마운트, cloudwatch로 로그 수집)

--- src/config/winston/winston.config.ts
+++ src/config/winston/winston.config.ts
@@ -1,10 +1,5 @@
-import * as winston from 'winston';
-import * as winstonDaily from 'winston-daily-rotate-file'; // 날짜별로 윈스턴 로그 저장 기능을 지원하는 모듈
 import { utilities, WinstonModuleOptions } from 'nest-winston';
-import { join } from 'path';
-
-const baseDir = join(process.cwd(), 'logs'); // ./logs
-const errLogDir = join(baseDir, 'error'); // ./logs/error
+import * as winston from 'winston';
 
 /**
  * winston에 설정할 커스텀 로그 우선순위 목록 정의
@@ -20,51 +15,18 @@ const levels = {
 };
 
 /**
- * 개발 환경 로그 저장위치 설정 => 콘솔에 출력
- * @returns
- */
-const devTransports = () => [new winston.transports.Console()];
-
-/**
- * 운영 환경 로그 저장 위치 설정 => 파일로 저장
- * @returns
- */
-const prodTransports = () => [
-  /* warn 레벨 이하 로그를 저장할 파일 설정 */
-  new winstonDaily({
-    level: 'warn',
-    datePattern: 'YYYY-MM-DD', // 파일명 날짜 형식 패턴
-    dirname: baseDir, // 저장 위치
-    filename: `%DATE%.log`, // ex) 2021-12-11.log
-    maxFiles: 30, // 30일치 로그 파일 저장
-    zippedArchive: true, // 30일이 지난 로그는 압축하도록 설정.
-  }),
-  /* error 레벨 로그를 저장할 파일 설정 */
-  new winstonDaily({
-    level: 'error',
-    datePattern: 'YYYY-MM-DD',
-    dirname: errLogDir,
-    filename: `%DATE%.error.log`, // ex) 2021-12-11.error.log
-    maxFiles: 30,
-    zippedArchive: true,
-  }),
-];
-
-/**
  * WinstonModule.createLogger()에 넣을 옵션을 생성하는 객체
- * - Production: File
- * - Development: Console
  */
 export const Winston = {
   getProductionConfig: (appName: string): WinstonModuleOptions => ({
     levels,
-    level: 'warn',
+    level: 'http',
     format: winston.format.combine(
       winston.format.timestamp(),
       winston.format.ms(),
-      utilities.format.nestLike(appName, { prettyPrint: true }),
+      utilities.format.nestLike(appName, { prettyPrint: true, colors: true }),
     ),
-    transports: prodTransports(),
+    transports: new winston.transports.Console(),
   }),
 
   getDevelopmentConfig: (appName: string): WinstonModuleOptions => ({
@@ -80,6 +42,6 @@ export const Winston = {
       utilities.format.nestLike(appName, { prettyPrint: true, colors: true }), // colors: false => 색상 없음
     ),
     /* 생성한 로그를 어디에 출력(전송)할지 설정 */
-    transports: devTransports(),
+    transports: new winston.transports.Console(),
   }),
 };
