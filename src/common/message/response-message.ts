1130758d2699fece22d2a4292ef03796a160c845
jae hyun <1994dbwogus@gmail.com>
2023-10-17 03:48:49 +0900

    style: error 메세지 문구 변경

--- src/common/message/response-message.ts
+++ src/common/message/response-message.ts
@@ -43,8 +43,7 @@ const feedErrorMessage = {
 export const errorMessage = {
   E400_APP_001: '요청이 유효성 검사를 통과하지 못하였습니다.',
   E404_APP_001: '요청한 자원이 존재하지 않거나 사용할 수 없습니다.',
-  E401_APP_001:
-    '인증 정보가 잘못되었습니다.(토큰 만료, 토큰 변조, 존재하지 않는 유저 등)',
+  E401_APP_001: '인증 정보가 잘못되었습니다.',
   E415_APP_001: '지원하지 않는 미디어 타입(mimetypes)입니다.',
   ...feedErrorMessage,
 };
