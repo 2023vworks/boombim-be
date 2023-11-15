af81cf52ddfc10bd2e9f3cdb84308edee24b15d5
jae hyun <1994dbwogus@gmail.com>
2023-11-15 13:30:05 +0900

    feat: PolygonInfo Entity 신규 추가

--- src/entity/geo/index.ts
+++ src/entity/geo/index.ts
@@ -2,3 +2,4 @@ export * from './geo-mark.entity';
 export * from './region-info.entity';
 export * from './address.entity';
 export * from './road-address.entity';
+export * from './polygon.entity';
