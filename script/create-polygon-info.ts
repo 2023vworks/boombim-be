ddcf0d81885ec2d82624e67f934cf368a191bf6a
jae hyun <1994dbwogus@gmail.com>
2023-11-15 23:52:46 +0900

    chore:  서울시 PolygonInfo 데이터 추가 스크립트 신규 추가

---
+++ script/create-polygon-info.ts
@@ -0,0 +1,45 @@
+import { DataSource } from 'typeorm';
+import * as fs from 'fs';
+import * as path from 'path';
+import { runOrm } from './run-orm';
+import { Geo } from './polygon/geo.type';
+import { PolygonInfo, RegionType } from '@app/entity';
+
+runOrm(async (dataSource: DataSource) => {
+  // 테스트 데이터 준비
+  const seoulGeoDatas = getSeoulGeoDatas('seoul-city.geo-35%.geo.json');
+
+  // polygon entity 생성
+  const polygonRepo = dataSource.getRepository(PolygonInfo);
+  const polygons = seoulGeoDatas.map(({ properties, geometry }) => {
+    return polygonRepo.create({
+      addressPart: properties.adm_nm,
+      city: properties.sidonm,
+      gu: properties.sggnm,
+      dong: properties.temp.split(' ')[1],
+      regionType: RegionType.H,
+      polygon: {
+        type: 'Polygon',
+        coordinates: geometry.coordinates,
+      },
+    });
+  });
+  await polygonRepo.save(polygons);
+  // NOTE: DBMS가 폴리곤이 저장된 테이블에 통계 수집하게 하여 쿼리 최적화를 수행한다.
+  await polygonRepo.query('ANALYZE polygon_info;');
+});
+
+function readJsonFile(filename: string) {
+  const jsonFillPath = path.join(__dirname, 'polygon', filename);
+  const json = fs.readFileSync(jsonFillPath, 'utf-8');
+  return JSON.parse(json);
+}
+
+/**
+ * geoJson에서 폴리곤 데이터 가져오기
+ * @param filename
+ * @returns
+ */
+function getSeoulGeoDatas(filename: string): Geo[] {
+  return readJsonFile(filename).features;
+}
