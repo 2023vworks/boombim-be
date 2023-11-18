cd4b3c4362f61a00e9b255dd9de07ace14c8fdef
jae hyun <1994dbwogus@gmail.com>
2023-11-18 12:33:54 +0900

    feat: POST /users body 신규 데이터 추가
    - agreementTerms

--- src/domain/user/dto/request/post-users-request.dto.ts
+++ src/domain/user/dto/request/post-users-request.dto.ts
@@ -1,5 +1,18 @@
-import { PickType } from '@nestjs/swagger';
+import { ApiProperty, PickType } from '@nestjs/swagger';
 
+import { BooleanValidator } from '@app/common';
 import { UserEntity } from '@app/entity';
+import { Expose } from 'class-transformer';
+import { Equals } from 'class-validator';
 
-export class PostUsersRequestDTO extends PickType(UserEntity, ['mbtiType']) {}
+export class PostUsersRequestDTO extends PickType(UserEntity, ['mbtiType']) {
+  @ApiProperty({
+    description: '약관 동의(false 불가)',
+    type: Boolean,
+    default: true,
+  })
+  @Expose()
+  @BooleanValidator()
+  @Equals(true)
+  agreementTerms: true;
+}
