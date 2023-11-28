a2811d2b8f6737fb837a07dc0b911b648fabce63
jae hyun <1994dbwogus@gmail.com>
2023-11-29 03:40:50 +0900

    style: UserEntity 주석 수정

--- src/entity/user/user.entity.ts
+++ src/entity/user/user.entity.ts
@@ -117,7 +117,7 @@ export class UserEntity extends BaseEntity {
   /* ========== 단순 연관관계 - 역방향 x ==========*/
   /**
    * 유저가 작성한 피드 내역
-   * - TODO: 유저가 삭제되면 피드도 삭제할까?
+   * - 유저는 자신의 피드 soft delete에만 권한을 가진다, 연관관계 주인은 Feed이다.
    */
   @ApiHideProperty()
   @Exclude()
@@ -129,14 +129,13 @@ export class UserEntity extends BaseEntity {
 
   /**
    * 유저가 작성한 댓글 내역
-   * - TODO: 유저가 삭제되면 댓글도 삭제할까?
+   * - 유저는 자신의 댓글 soft delete에만 권한을 가진다, 연관관계 주인은 Feed이다.
    */
   @ApiHideProperty()
   @Exclude()
   @OneToMany(() => CommentEntity, (comment) => comment.user, {
     nullable: true,
     cascade: ['soft-remove'],
-    // Note: 유저는 자신의 댓글 삭제에만 권한을 가진다, 연관관계 주인은 Feed이다.
   })
   comments: CommentEntity[];
 
